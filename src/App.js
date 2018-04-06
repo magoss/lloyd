a3d517560dc5640ffd3df335896644eedab7a679
CORP\MGOSCINI <marcingosciniak@poczta.fm>
2018-04-06 14:11:48 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
         <code>
-          &lt;magos&gt;
+          &lt;magoss&gt;
         </code>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
